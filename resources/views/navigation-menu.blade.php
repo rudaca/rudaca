59e010b6bb28df5144b84a645922b2e04551bead
developer <developer@ellisontravel.com>
2022-01-28 22:46:54 +0530

    RUD-36 updating code

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -120,7 +120,7 @@
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
 
-                                <x-jet-dropdown-link href="{{ route('logout') }}"
+                                <x-jet-dropdown-link href="{{ url('/logout') }}"
                                          onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                     {{ __('Log Out') }}
