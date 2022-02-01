2bef4dd6343845235ce977327e15e8362aa012fe
developer <developer@ellisontravel.com>
2022-02-01 14:22:00 +0530

    RUD-35 updating code

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://devrudaca-hqcaj.ondigitalocean.app'),
 
     'asset_url' => env('ASSET_URL', null),
 
