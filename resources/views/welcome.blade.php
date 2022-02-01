f021213189fe433ed86d4f6aa1b7dbb3e6129f9d
developer <developer@ellisontravel.com>
2022-02-01 12:44:11 +0530

    RUD-35 updating code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
 			
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-				<h1>Rudaca</h1>
+				<h1>Rudaca.</h1>
                 </div>
 
                 <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
