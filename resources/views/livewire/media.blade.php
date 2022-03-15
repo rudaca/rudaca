54834fc9f59cb45bda9ce7e7912352af035a4c02
developer <developer@ellisontravel.com>
2022-03-15 23:41:32 +0530

    RUD-34 updating code

--- resources/views/livewire/media.blade.php
+++ resources/views/livewire/media.blade.php
@@ -38,6 +38,12 @@
 						@endif
 					</h2>
 
+					@if((is_object($media) && strstr($media->mime_type,"video/")) || (isset($media['mime_type']) && strstr($media['mime_type'],"video/")))
+					<p class="clear-both mt-0 text-gray-800">
+						&nbsp;
+					</p>
+					@endif
+
 					<p class="clear-both mt-2 text-gray-800">
 							<svg xmlns="http://www.w3.org/2000/svg" class="float-left h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 							  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
