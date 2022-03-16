b0ba9c69ec5f96af340d61b532c0c88946bb69c6
developer <developer@ellisontravel.com>
2022-03-16 22:44:07 +0530

    RUD-34 updating code

--- resources/views/livewire/media.blade.php
+++ resources/views/livewire/media.blade.php
@@ -96,6 +96,10 @@
 						</ul>
 					  </section>
 					</div>
+					
+					<div class="flex justify-end">
+					  <div><button type="button" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button></div>
+					</div>
 				</a>
         @endforeach
 		@endif
