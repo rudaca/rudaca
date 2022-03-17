8fc81914d7347c3e13a6a834cd2177e8f6fd0d58
developer <developer@ellisontravel.com>
2022-03-17 23:22:29 +0530

    RUD-35 updating code

--- resources/views/livewire/upload-files.blade.php
+++ resources/views/livewire/upload-files.blade.php
@@ -49,7 +49,7 @@
 							  <div class="md:flex md:items-center">
 								<div class="md:w-1/3"></div>
 								<div class="md:w-2/3">
-								  <button class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
+								  <button wire:loading.class.remove="shadow bg-blue-500 hover:bg-blue-400"  wire:loading.class="bg-gray" class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
 									Upload
 								  </button>
 								</div>
