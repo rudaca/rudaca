91930b52b46eeb309338f9d528252424d5ad4a05
developer <developer@ellisontravel.com>
2022-03-19 01:22:35 +0530

    RUD-34 updating code

--- app/Http/Livewire/Media.php
+++ app/Http/Livewire/Media.php
@@ -49,11 +49,7 @@ class Media extends Component
 				$media->delete();
 			}
 		}
-		
 		$list = UploadFile::where('user_id',Auth::user()->id)->latest()->paginate($this->perPage, ['*'], 'page', $this->pageNumber);
-        $this->pageNumber += 1;
-        $this->hasMorePages = $list->hasMorePages();
-        $this->media_list->push(...$list->items());
-		$this->emit('render');
+		$this->media_list = [];
 	}
 }
