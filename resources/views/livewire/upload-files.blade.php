fabc1d07e98c3880b0ee64ec2d012c943fddf172
developer <developer@ellisontravel.com>
2022-03-19 17:22:27 +0530

    RUD-34 updating code

--- resources/views/livewire/upload-files.blade.php
+++ resources/views/livewire/upload-files.blade.php
@@ -22,7 +22,8 @@
 					  <p class="text-sm">Uploading Media Images...</p>
 					</div>
 					
-					<div wire:loading wire:target="submit" class="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3" role="alert">
+					<div wire:loading wire:target="submit" class="min-w-full bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3" role="alert">
+					  <p class="font-bold">Media alert</p>	
 					  <p class="font-bold">Selected files are uploading to server please wait...</p>
 					</div>
 					
