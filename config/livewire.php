128118e17bc7c2d24c0fcf9b6e01e04205b1057a
developer <developer@ellisontravel.com>
2022-06-18 16:40:16 +0530

    updating code for large files

--- config/livewire.php
+++ config/livewire.php
@@ -2,6 +2,6 @@
 return [
     'temporary_file_upload' => [
         'disk' => 'public',
-		'rules' => 'file|mimes:png,jpg,jpeg,gif,mp4|max:2002400',
+		'rules' => 'file|mimes:png,jpg,jpeg,gif,mp4|max:2002400',  // 102400
     ],
 ];
